3b1f775a6f87f1198688a9a7553ff8fa94297c84
glenbuck503 <glenbuck503@gmail.com>
2021-03-02 09:21:58 -0800

    working puppy

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -1,29 +1,39 @@
 using Microsoft.AspNetCore.Mvc;
 using MadLibs.Models;
+using PuppyMadLibs.Models;
 
 namespace MadLibs.Controllers
 {
   public class HomeController : Controller
   {
+    [Route("/")]
+    public ActionResult SelectStory() { return View(); }
+
     [Route("/form")]
     public ActionResult Form() { return View(); }
-  
 
-  [Route("/results")]
+    [Route("/puppyform")]
+    public ActionResult PuppyForm() { return View(); }
+
+    [Route("/results")]
     public ActionResult Results(string noun, string verb)
     {
       MadLib myLetterVariable = new MadLib();
       myLetterVariable.Noun = noun;
       myLetterVariable.Verb = verb;
-
+  
       return View(myLetterVariable);
     }
 
-  //   [Route("/puppy-story")]
-  //   public ActionResult Puppy()
-  //   {
-      
-  //   }
-  // }
+    [Route("/puppy")]
+    public ActionResult Puppy(string puppy, string breed)
+    {
+      PuppyMadLib puppyVariable = new PuppyMadLib();
+    
+     
+      puppyVariable.Puppy = puppy;
+      puppyVariable.Breed = breed;
+      return View(puppyVariable);
+    }
   }
 }
---
+++ MadLibs/Models/PuppyMadLib.cs
@@ -0,0 +1,10 @@
+namespace PuppyMadLibs.Models
+{
+  public class PuppyMadLib
+  {
+    public string Puppy { get; set; }
+    public string Breed { get; set; }
+    
+
+  }
+}
